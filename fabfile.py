dc6dbd526cbd4c73fa10e1357eced997615ca573
Piotr Fidkowski <piotr@fidkowski.com>
2013-09-29 23:31:05 -0700

    Added fabfile for deployment

---
+++ fabfile.py
@@ -0,0 +1,18 @@
+from fabric.api import *
+
+# Hosts to deploy onto.
+env.hosts = ['battlef6@battleforboston.com']
+
+env.project_root = '/home8/battlef6/mysite'
+
+def prepare_deploy():
+  local('python manage.py test tourny')
+  local('git add -p && git commit')
+  local('git push')
+
+def deploy():
+  with cd(env.project_root):
+    run('git pull')
+    run('python manage.py migrate tourny')
+    run('python manage.py collectstatic -v0 --noinput')
+    run('touch /home8/battlef6/django.fcgi')
